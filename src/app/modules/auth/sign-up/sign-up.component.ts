cbd35133a415783406de6eead00be362bd27bc36
Vũ Văn Minh <vanminh.vu@2bsystem.com.vn>
2024-09-04 16:53:44 +0700

    Refactor navbar layout and search functionality in conversation list, address book, dashboard, and film components

--- src/app/modules/auth/sign-up/sign-up.component.ts
+++ src/app/modules/auth/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Validators } from '@angular/forms';
 import { BaseComponent } from 'src/app/core/base/base.component';
 import { ShareModule } from 'src/app/core/share/share.module';
 
@@ -13,4 +14,13 @@ import { ShareModule } from 'src/app/core/share/share.module';
   templateUrl: './sign-up.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SignUpComponent extends BaseComponent { }
+export class SignUpComponent extends BaseComponent {
+  override ngOnInit(): void {
+    super.ngOnInit();
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+}
